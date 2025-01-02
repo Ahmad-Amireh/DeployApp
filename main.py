05a65dd5d8184093eb4fe7e95daa51574abea528
Ahmad-Amireh <aamerh21@gmail.com>
2025-01-02 22:25:35 +0300

    add api key

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from prediction import prediction
 from pydantic import BaseModel
+from fastapi.security.api_key import APIKeyHeader
 
 # Initialize FastAPI
 app = FastAPI()
@@ -18,12 +19,26 @@ class IrisInput(BaseModel):
     petal_length: float
     petal_width: float
 
+
+API_KEY = "DUK_90"
+API_KEY_NAME = "API-Key"
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=True)
+# Security dependency
+def verify_api_key(api_key: str = Depends(api_key_header)):
+    if api_key != API_KEY:
+        raise HTTPException(status_code=403, detail="Invalid API key")
+
+# Example route with API key protection
+@app.get("/secure-data", dependencies=[Depends(verify_api_key)])
+async def get_secure_data():
+    return {"message": "This is secured data"}
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the FastAPI Prediction Service"}
 
 @app.post('/predict/')
-def make_prediction(input_data: IrisInput):
+async def make_prediction(input_data: IrisInput, api_key: str = Depends(verify_api_key)):
     try: 
         # Prepare the input for the model
         input_features = [
@@ -37,4 +52,3 @@ def make_prediction(input_data: IrisInput):
         return {"input": input_data.dict(), "prediction": LABELS[prediction[0]]}
     except Exception as e: 
         raise HTTPException(status_code=500, detail=str(e))
-
